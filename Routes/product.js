360a6b50019bcbcbd4c7c94bc8f98e29642c3eb2
Danish Surah <mr.surah12345@gmail.com>
2025-07-23 16:29:23 +0530

    first commit

---
+++ Routes/product.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  addProduct,
+  deleteProductById,
+  getAllProducts,
+  getProductById,
+  updaeteProductById,
+} from "../Controllers/products.js";
+import {Authenticate} from '../Middlewares/auth.js'
+
+const router = express.Router();
+
+// add Products
+router.post("/add",Authenticate, addProduct);
+
+// get All Products
+router.get("/all", getAllProducts);
+
+// get Product by Id
+router.get("/:id", getProductById);
+
+// update product by Id
+router.put("/:id",Authenticate,updaeteProductById);
+
+// delete product by Id
+router.delete("/:id",Authenticate,deleteProductById)
+
+export default router;
---
+++ server.js
@@ -0,0 +1,57 @@
+import express from 'express'
+
+import mongoose from 'mongoose';
+import userRouter from './Routes/user.js'
+import productRouter from './Routes/product.js'
+import cartRouter from './Routes/cart.js'
+import addressRouter from './Routes/address.js'
+import paymentRouter from  './Routes/payment.js'
+import cors from 'cors'
+import { config } from 'dotenv';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json())
+
+// .env setup
+
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["POST", "GET", "DELETE", "PUT"],
+  credentials: true
+}));
+
+
+app.get('/',(req,res)=>{res.json({message:'this is home page'})}) 
+
+// user Router
+app.use('/api/user',userRouter) 
+
+// product Router
+app.use('/api/product',productRouter);
+
+// cart Router
+app.use('/api/cart',cartRouter);
+
+// address Router
+app.use('/api/address',addressRouter);
+
+// payment Router
+app.use('/api/payment',paymentRouter)
+
+
+mongoose
+  .connect(
+    'mongodb+srv://mrsurah12345:xRP3nNGcJUNrTCl4@cluster0.na2ugda.mongodb.net/',
+    {
+      dbName: "MERN_Ecommerce",
+    }
+  )
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 1000;
+app.listen(port,()=>console.log(`Server is running`))
